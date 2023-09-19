69dc89804f65f3daf4ec6d9d2335ca90f4ff196a
Lucas Matos <lucaslmatos18@gmail.com>
2023-09-19 15:28:14 -0300

    Req1 - Tentativa inicial

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,5 +1,8 @@
 package com.trybe.java.regraprogressao;
 
+import java.util.Scanner;
+
+
 /**
  * App.
  */
@@ -9,5 +12,20 @@ public class App {
    * Metodo main.
    */
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Digite a quantidade de atividades para cadastrar: \n");
+    String strqtdAtividades = scanner.next();
+    Integer qtdInteger = Integer.parseInt(strqtdAtividades);
+    Integer contador = 1;
+
+    do {
+      System.out.print("Digite o nome da atividade " + contador + ": \n");
+      String strAtividades = scanner.next();
+      System.out.print("Digite o peso da atividade " + contador + ": \n");
+      String strPesos = scanner.next();
+      contador++;
+
+    } while (contador <= qtdInteger);
+    scanner.close();
   }
 }
