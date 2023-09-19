dc96bee8606e2048d4a4b78e974b6582c45c9b32
Lucas Matos <lucaslmatos18@gmail.com>
2023-09-19 16:46:07 -0300

    Tentativa 2

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -13,19 +13,37 @@ public class App {
    */
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    System.out.print("Digite a quantidade de atividades para cadastrar: \n");
+    System.out.println("Digite a quantidade de atividades para cadastrar: ");
     String strqtdAtividades = scanner.next();
     Integer qtdInteger = Integer.parseInt(strqtdAtividades);
     Integer contador = 1;
+    Integer pesoXNota = 0;
+    Integer peso = 0;
 
     do {
-      System.out.print("Digite o nome da atividade " + contador + ": \n");
-      String strAtividades = scanner.next();
-      System.out.print("Digite o peso da atividade " + contador + ": \n");
-      String strPesos = scanner.next();
+      System.out.println("Digite o nome da atividade " + contador + ": ");
+      String strAtividade = scanner.nextLine();
+      scanner.nextLine();
+      System.out.println("Digite o peso da atividade " + contador + ": ");
+      Integer strPeso = scanner.nextInt();
+      System.out.println("Digite a nota obtida para " + strAtividade + ": ");
+      Integer strNota = scanner.nextInt();
+      pesoXNota = pesoXNota + (strNota * strPeso);
+      peso = peso + strPeso;
       contador++;
 
     } while (contador <= qtdInteger);
+
     scanner.close();
+    double result = (pesoXNota / peso);
+
+    if (result >= 85) {
+      System.out.println("Parabéns! Você alcançou " 
+          + result + "%! E temos o prazer de informar que você obteve aprovação! ");
+    } else {
+      System.out.println("Lamentamos informar que, com base na sua pontuação" 
+          +  "alcançada neste período, " 
+          + result + "%, você não atingiu a pontuação mínima necessária para sua aprovação.");
+    }
   }
 }
